f2c865c22b47675a41b57b88010e529bb7c25591
Vir1dian <132039432+Vir1dian@users.noreply.github.com>
2023-05-17 00:49:01 -0700

    Countdown timer WIP
    Countdown function is working, but there is still no proper way to input the desired time at the moment.

--- chef.js
+++ chef.js
@@ -15,3 +15,76 @@ function search() { //Finds recipes as the user types
         }
     }
 }
+
+var seconds = 300;
+var countingTime = null;
+document.getElementById("timer").innerHTML = "00:05:00";
+var originalTimeShown = document.getElementById("timer").innerHTML;
+var originalTimeInSeconds = seconds;
+
+
+/*function countup() {
+    seconds++;
+    let hrs = Math.floor(seconds/3600);
+    if (hrs/10 < 1) {
+        hrs = "0" + hrs;
+    }
+    let mins = Math.floor(seconds/60) % 60;
+    if (mins/10 < 1) {
+        mins = "0" + mins;
+    }
+    let secs = seconds % 60;
+    if (secs/10 < 1) {
+        secs = "0" + secs;
+    }
+    document.getElementById("timer").innerHTML = hrs + ":" + mins + ":" + secs;
+}*/
+
+function countdown() {
+    if (seconds > 0) {
+        seconds--;
+    } else {
+        pauseTimer();
+        return;
+    }
+    let hrs = Math.floor(seconds/3600);
+    if (hrs/10 < 1) {
+        hrs = "0" + hrs;
+    }
+    let mins = Math.floor(seconds/60) % 60;
+    if (mins/10 < 1) {
+        mins = "0" + mins;
+    }
+    let secs = seconds % 60;
+    if (secs/10 < 1) {
+        secs = "0" + secs;
+    }
+    document.getElementById("timer").innerHTML = hrs + ":" + mins + ":" + secs;
+}
+function setTimer() {
+    document.getElementById("timer").style.display = "none";
+    document.getElementById("timerInput").style.display = "flex";
+}
+function commitTimer() {
+    if (e.keyCode == 13) {
+        document.getElementById("timer").style.display = "flex";
+        document.getElementById("timerInput").style.display = "none";
+    }
+    //WIP
+}
+function startTimer() {
+    if (countingTime) {
+        return;
+    }
+    countingTime = setInterval(countdown, 1000);
+}
+function pauseTimer() {
+    clearInterval(countingTime);
+    countingTime = null;
+}
+function resetTimer() {
+    clearInterval(countingTime);
+    countingTime = null;
+    document.getElementById("timer").innerHTML = originalTimeShown;
+    seconds = originalTimeInSeconds;
+}
